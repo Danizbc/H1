c258a82107ef8e958acab40fc5617c8cc7acfea7
Daniel Sørensen <danielftw321@gmail.com>
2019-01-29 13:22:29 +0100

    Opgave1.2

---
+++ MethodOPG1.2/MethodOPG1.2/Logic.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOPG1._2
+{
+    class Logic
+    {
+        //Attribute the storing place for my variables
+        double inputOne = 0;
+        double inputTwo = 0;
+        double result = 0;
+        bool runWhileTrue = true;
+        //Properties this is where i create get and set.
+        public double InputOne
+        {
+            get { return inputOne; }
+            set
+            {
+                inputOne = value;
+            }
+        }
+
+        public double InputTwo
+        {
+            get { return inputTwo; }
+            set
+            {
+                inputTwo = value;
+            }
+        }
+
+        public double Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+            }
+        }
+
+        public bool RunWhileTrue
+        {
+            get { return runWhileTrue; }
+            set
+            {
+                runWhileTrue = value;
+            }
+        }
+
+        //method 
+        public double Pythagoras()
+        {
+            result = (inputOne * inputOne + InputTwo * InputTwo);
+            return result;
+        }
+
+        /// <summary>
+        /// If true First input is bigger than secound input!
+        /// </summary>
+        /// <returns></returns>
+        public bool BiggestNumber()
+        {
+            if (InputOne > InputTwo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ MethodOPG1.2/MethodOPG1.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOPG1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating Class logic.
+            Logic logic = new Logic();
+
+         //Creating loop so it run untill set to false.
+            while (logic.RunWhileTrue == true)
+            {
+                //Trying to convert readline into double, if fail it will catch it and throw error message that something went wrong.
+                Console.Clear();
+                try
+                {
+                    Console.WriteLine("This program are made to calculate Pythagoras");
+                    Console.Write("Input number a = ");
+                    logic.InputOne = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Input number b = ");
+                    logic.InputTwo = Convert.ToDouble(Console.ReadLine());
+                    logic.RunWhileTrue = false;
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Something went wrong try again.\n\nPress any key to continue.");
+                    Console.ReadKey();
+                }
+            }
+
+            //Printing out Pythagoras by running method logic.Pythagoras inside class called logic
+            Console.WriteLine($"Result = {logic.Pythagoras()}");
+
+            //Running Bool method to see if a is bigger than b
+            if (logic.BiggestNumber() == true)
+            {
+                Console.WriteLine("Input a is bigger than b");
+            }
+            else
+            {
+                Console.WriteLine("Input B is bigger than A");
+            }
+
+            //Readkey so we have something to read, just kidding, to end program GG
+            Console.ReadKey();
+        }
+    }
+}
