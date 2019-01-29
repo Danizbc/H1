272b310433e816a4506d73ca7ddc5208ac73b266
Daniel Sørensen <danielftw321@gmail.com>
2019-01-29 12:37:07 +0100

    Method OPG 1.0

---
+++ Methods/Methods/Calculator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Calculator
+    {
+        //Attributes
+        double input = 0;
+        double inputTwo = 0;
+        double output = 0;
+    
+   
+
+        //Properties
+        public double Input
+        {
+            get { return input; }
+            set
+            {
+            input = value;
+            }
+        }
+        public double InputTwo
+        {
+            get { return inputTwo; }
+            set
+            {
+                inputTwo = value;
+            }
+        }
+        public double Output
+        {
+            get { return output; }
+            set
+            {
+                output = value;
+            }
+        }
+
+       public bool RunWhileTrue = true;
+
+
+
+        //Methods
+
+            //Devide and round up values.
+        public double Devider()
+        {
+            Output = Input / InputTwo;
+            output = Math.Round(output, 4);
+            //returning value
+            return output;
+        }
+
+        //Find Modus and round up to 4 digits.
+        public double Modus()
+        {
+            output = Input % InputTwo;
+            output = Math.Round(output, 4);
+            //returning value
+            return output;
+        }
+
+        //find potensenss and return value
+        public double Potensenss()
+        {
+            // output = input ^ InputTwo;
+           output = Math.Pow(input, InputTwo);
+            //returning value
+            return output;
+        }
+
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Getting access to class calculator by creating calc
+            Calculator Calc = new Calculator();
+
+            Console.WriteLine("This Program will devide two numbers, find modus & power and print it to you.");
+            //Adding two values of double to calc.input and calc.inputtwo
+            while (Calc.RunWhileTrue == true)
+            {
+                try
+                {
+                    Console.Write("Please input first number.\nNumber = ");
+                    Calc.Input = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please input secound number.\nNumber = ");
+                    Calc.InputTwo = Convert.ToDouble(Console.ReadLine());
+                    Calc.RunWhileTrue = false;
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Something went wrong.");
+                }
+                
+       
+            }
+
+            //Printing out 2 numbers devided, by running Calc.Devider Method
+            Console.WriteLine($"numbers devided  = {Calc.Devider()}");
+            //Printing modus of the 2 numbers by running method Calc.Modus method
+            Console.WriteLine($"Modus = {Calc.Modus()}");
+            //Printing out power of 2 numbers inputted by running Calc.Potensenss method
+            Console.WriteLine($"Potensens = {Calc.Potensenss()}");
+
+            Console.ReadKey();
+        }
+    }
+}
